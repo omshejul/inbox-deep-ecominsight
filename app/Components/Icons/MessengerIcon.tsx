8becf073cc87d445826381274e824dbb7046dbc5
Om Shejul <contact@omshejul.com>
2024-01-12 00:10:00 +0530

    Refactor Sidebar component icons

---
+++ app/Components/Icons/MessengerIcon.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const MessengerIcon = () => {
+  return (
+<svg height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+<defs>
+  <linearGradient
+    id="prefix__a"
+    y1="15.2"
+    x2="112.429"
+    x1="112.429"
+    y2="214.965"
+    gradientTransform="matrix(.10592 0 0 .10673 .091 0)"
+    gradientUnits="userSpaceOnUse"
+  >
+    <stop stop-color="#00C6FF" offset="0" />
+    <stop stop-color="#0068FF" offset="1" />
+  </linearGradient>
+</defs>
+<path
+  fill="url(#prefix__a)"
+  d="M4.478 19.726V24l3.997-2.272a12.72 12.72 0 003.525.494c6.577 0 11.909-4.975 11.909-11.11C23.909 4.974 18.577 0 12 0 5.423 0 .091 4.975.091 11.111c0 3.475 1.71 6.577 4.387 8.615z"
+/>
+<path
+  fill="#fff"
+  d="M10.78 8.068l-6.422 6.8 5.844-3.207 3.054 3.207 6.386-6.8-5.78 3.15z"
+/>
+</svg>
+  )
+}
+
+export default MessengerIcon
---
+++ app/Components/Icons/XIcon.tsx
@@ -0,0 +1,22 @@
+
+const XIcon = () => {
+  return (
+<svg
+shape-rendering="geometricPrecision"
+text-rendering="geometricPrecision"
+image-rendering="optimizeQuality"
+fill-rule="evenodd"
+clip-rule="evenodd"
+width="24"
+height="24"
+xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  fill-rule="nonzero"
+  d="M18.901 1.153h3.68l-8.04 9.19L24 22.847h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.829L0 1.153h7.594l5.243 6.932zM17.61 20.644h2.039L6.486 3.24H4.298z"
+/>
+</svg>
+  )
+}
+
+export default XIcon
